ddfa80cee9b5cbfb5c5abf01358e206fbe3e275f
Thomas-Raveneau <thomas.raveneau1@gmail.com>
2025-05-16 16:56:01 +0200

    fix

--- scenes/gameplay/game/spawn/spawn_point.gd
+++ scenes/gameplay/game/spawn/spawn_point.gd
@@ -3,10 +3,12 @@ extends Node3D
 
 @onready var spawn_timer: Timer = $SpawnTimer
 
-@export var is_spawnable: bool = true
+var is_spawnable: bool = true
 var spawn_delay_seconds: int = 2
 
 func _ready() -> void:
+	if !multiplayer.is_server():
+		return
 	spawn_timer.one_shot = true
 	spawn_timer.autostart = false
 	spawn_timer.timeout.connect(_on_spawn_delay_timeout)
